f90b67f8c6397d678f6f3e940c8d4960fd0f16bc
shahsajib95 <sajib.shahariar97@gmail.com>
2023-09-07 18:03:29 +0600

    api changed

--- assets/AppWrapper-0a639d0f.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-4fc1e700.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/PrivateOutlet-b3e56826.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as t,N as u}from"./index-4fc1e700.js";import{u as i,N as n}from"./useAuth-d2af15bd.js";const l=({children:e})=>{const[s,r]=o.useState(!1),{loggedIn:a}=i();return a?t.jsxs(t.Fragment,{children:[t.jsx(n,{setShowSideBar:r,showSidebar:s,userView:!0}),e]}):t.jsx(u,{to:"/account/login"})};export{l as default};
