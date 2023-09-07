d6ca12c524ca68681b2ec5c7c2383f92ba344b61
shahsajib95 <sajib.shahariar97@gmail.com>
2023-09-07 17:52:55 +0600

    department, workflow search fixed

---
+++ assets/AppWrapper-0a639d0f.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-4fc1e700.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-6e625e95.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-375cfac8.js";const s=()=>t.jsx(r,{});export{s as default};
