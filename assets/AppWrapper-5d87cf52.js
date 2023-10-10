4f1a19802b5d98ed1397a19bf45598a7f538ac0e
shahsajib95 <sajib.shahariar97@gmail.com>
2023-10-10 16:28:11 +0600

    ui change

---
+++ assets/AppWrapper-5d87cf52.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-504ad2c2.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-7267f772.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-ea7a40d0.js";const s=()=>t.jsx(r,{});export{s as default};
