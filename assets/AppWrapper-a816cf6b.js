4bad0be4cc5a3fb920d051fb15ea8d1fdcff850e
shahsajib95 <sajib.shahariar97@gmail.com>
2023-10-10 16:17:45 +0600

    bug fixed in application and department

--- assets/AppWrapper-a816cf6b.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-8f24b297.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/PrivateOutlet-0ccdef13.js
@@ -0,0 +1,1 @@
+import{r as o,j as t,N as u}from"./index-ea7a40d0.js";import{u as i,N as n}from"./useAuth-8cc2a9a7.js";const l=({children:e})=>{const[s,r]=o.useState(!1),{loggedIn:a}=i();return a?t.jsxs(t.Fragment,{children:[t.jsx(n,{setShowSideBar:r,showSidebar:s,userView:!0}),e]}):t.jsx(u,{to:"/account/login"})};export{l as default};
