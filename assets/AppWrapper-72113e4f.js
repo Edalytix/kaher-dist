ca545f0f8569530e716d3bddd9fe168fffd84d03
shahsajib95 <sajib.shahariar97@gmail.com>
2023-10-11 17:31:17 +0600

    fix: bugs department-user,empty field application

---
+++ assets/AppWrapper-72113e4f.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-30ec9630.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-da434607.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-7ed315b1.js";const s=()=>t.jsx(r,{});export{s as default};
