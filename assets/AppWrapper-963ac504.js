348dc015fc0229ef4d801d3821faca539780d4af
shahsajib95 <sajib.shahariar97@gmail.com>
2023-10-26 17:18:13 +0600

    type role change

---
+++ assets/AppWrapper-963ac504.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-c1ef5455.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-fc0abac3.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-c254ba90.js";const s=()=>t.jsx(r,{});export{s as default};
