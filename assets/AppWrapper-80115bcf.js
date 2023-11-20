6574db8671c2c97e030c472b59812e0f49a7ed4e
shahsajib95 <sajib.shahariar97@gmail.com>
2023-11-20 15:30:18 +0600

    fix: page box issue

--- assets/AppWrapper-80115bcf.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-904401b9.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/PrivateOutlet-a8eba42e.js
@@ -0,0 +1,1 @@
+import{r as o,j as t,N as i}from"./index-e64a0dea.js";import{u as n,N as u}from"./useAuth-e5daf2ba.js";const l=({children:s})=>{const[e,a]=o.useState(!1),{loggedIn:r}=n();return r?t.jsxs(t.Fragment,{children:[t.jsx(u,{setShowSideBar:a,showSidebar:e,userView:!0}),t.jsx("div",{className:"container",children:s})]}):t.jsx(i,{to:"/account/login"})};export{l as default};
