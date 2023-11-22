6b34a6b94c9e8a860551ebae110df027594eb8a8
shahsajib95 <sajib.shahariar97@gmail.com>
2023-11-22 21:34:15 +0600

    fix: card bug

--- assets/AppWrapper-13efc33c.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-64825f53.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-e353d5fd.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-2f3aa69f.js";const s=()=>t.jsx(r,{});export{s as default};
