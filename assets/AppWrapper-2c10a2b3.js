35cf28b88a8ea5f42b121033847dc0bafdcd9aed
shahsajib95 <sajib.shahariar97@gmail.com>
2023-11-24 11:38:11 +0600

    fix inst principal

---
+++ assets/AppWrapper-2c10a2b3.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-a9618b4d.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-e353d5fd.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-2f3aa69f.js";const s=()=>t.jsx(r,{});export{s as default};
