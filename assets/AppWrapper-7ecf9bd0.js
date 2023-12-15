711c43de9f25895a5cc571652289899d58951efe
shahsajib95 <sajib.shahariar97@gmail.com>
2023-12-15 15:12:23 +0600

    changes

--- assets/AppWrapper-7ecf9bd0.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-f54cb665.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-ef3c08d0.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-c24926ff.js";const s=()=>t.jsx(r,{});export{s as default};
