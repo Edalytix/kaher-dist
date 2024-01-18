89ef47937829b9a75876a549fc8350590c48c6e0
shahsajib95 <sajib.shahariar97@gmail.com>
2024-01-18 12:04:14 +0600

    reports admin application added

--- assets/AppWrapper-037f3e07.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-ad909450.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-a92a6b06.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-b998a78c.js";const s=()=>t.jsx(r,{});export{s as default};
