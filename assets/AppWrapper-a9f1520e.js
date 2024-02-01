5089f4da16eabcdd385845cd64581e91492f3383
shahsajib95 <sajib.shahariar97@gmail.com>
2024-02-01 17:49:39 +0600

    fix file isue

---
+++ assets/AppWrapper-a9f1520e.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-471a47cd.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-ae0a3e7e.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-73d5be56.js";const s=()=>t.jsx(r,{});export{s as default};
