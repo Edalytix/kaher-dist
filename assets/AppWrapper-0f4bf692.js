9b2e0b9bd30fa0c042e2d20d4b008e63cddd6ac7
shahsajib95 <sajib.shahariar97@gmail.com>
2024-02-27 15:54:34 +0600

    ui updated dashboard

--- assets/AppWrapper-0f4bf692.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-9ff3e86f.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-f3d730a1.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-603db4ba.js";const s=()=>t.jsx(r,{});export{s as default};
