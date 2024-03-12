00d866bcba0498282463aae4644f7496070b3b17
shahsajib95 <sajib.shahariar97@gmail.com>
2024-03-12 19:06:01 +0600

    validation: when resend email

---
+++ assets/AppWrapper-88ee2850.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-33ed9892.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-a21f6357.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-1c29f78f.js";const s=()=>t.jsx(r,{});export{s as default};
