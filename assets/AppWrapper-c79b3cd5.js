4ca8274b8382d4e9f505f0acad86780ff2e2ebe0
shahsajib95 <sajib.shahariar97@gmail.com>
2024-03-12 14:28:31 +0600

    color, application submitted last

--- assets/AppWrapper-c79b3cd5.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-df9d1e6c.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-d120dc71.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-e8c20615.js";const s=()=>t.jsx(r,{});export{s as default};
