36e6b588bd89072dde366532670ed44d12ff2041
shahsajib95 <sajib.shahariar97@gmail.com>
2024-05-09 12:48:19 +0600

    fix: pdf

--- assets/AppWrapper-37ae784b.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-d3e99d1b.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-628b8a9a.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-a7775fe0.js";const s=()=>t.jsx(r,{});export{s as default};
