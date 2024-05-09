244704e9488ee6cb96feda9caf74d159e6f78f94
shahsajib95 <sajib.shahariar97@gmail.com>
2024-05-09 15:51:17 +0600

    change url

--- assets/AppWrapper-068468f0.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-7f59a2ab.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-5d73d535.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-87dd9dcc.js";const s=()=>t.jsx(r,{});export{s as default};
