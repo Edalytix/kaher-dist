e96d8c0c36b495df106f28aac7ca46226656abb5
shahsajib95 <sajib.shahariar97@gmail.com>
2024-05-09 15:30:35 +0600

    fix status

---
+++ assets/AppWrapper-068468f0.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-7f59a2ab.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-628b8a9a.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-a7775fe0.js";const s=()=>t.jsx(r,{});export{s as default};
