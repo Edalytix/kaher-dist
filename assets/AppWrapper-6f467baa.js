bda41bb936e1b48657bdd90f4b44378d9c8b2dcf
shahsajib95 <sajib.shahariar97@gmail.com>
2024-07-04 20:36:15 +0600

    fix: table

--- assets/AppWrapper-6f467baa.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-3b9f7185.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-e749d868.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-85134cce.js";const s=()=>t.jsx(r,{});export{s as default};
