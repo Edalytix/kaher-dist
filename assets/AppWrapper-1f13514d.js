35fcfba0a9aa88243fcaf714d4526574228d22b8
shahsajib95 <sajib.shahariar97@gmail.com>
2025-02-17 17:24:55 +0600

    fix: lazy loading

--- assets/AppWrapper-1f13514d.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-ffbf116d.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-bbe32b3b.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-a089f349.js";const s=()=>t.jsx(r,{});export{s as default};
