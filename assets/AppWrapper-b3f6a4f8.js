b97c5810fd1e1df3e96575268276c146a84c2efe
shahsajib95 <sajib.shahariar97@gmail.com>
2025-06-02 11:51:44 +0600

    fix: loazy load

--- assets/AppWrapper-b3f6a4f8.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-917ea966.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-e3695c02.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-6b8422ba.js";const s=()=>t.jsx(r,{});export{s as default};
