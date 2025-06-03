98a3fdd5e48a12a1b9221a9d31fddce536dc8afa
shahsajib95 <sajib.shahariar97@gmail.com>
2025-06-03 14:46:18 +0600

    fix: admission

---
+++ assets/AppWrapper-1b3acdf4.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-15c72f77.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-35a86918.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-df57e246.js";const s=()=>t.jsx(r,{});export{s as default};
