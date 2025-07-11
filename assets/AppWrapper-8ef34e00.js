d1900483e1e0c5284912e534fe1b202e7a1235fc
shahsajib95 <sajib.shahariar97@gmail.com>
2025-07-11 14:19:55 +0600

    fix: admin role validation -prod

---
+++ assets/AppWrapper-8ef34e00.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-cb55bb7d.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-9d81dc81.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-a93bd74f.js";const s=()=>t.jsx(r,{});export{s as default};
