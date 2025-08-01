4db8ba89f998c3fbbd66e5b38c2c23f2c74a69e1
shahsajib95 <sajib.shahariar97@gmail.com>
2025-08-01 13:59:30 +0600

    fix: institution courses, access in protocol and workflow and application update route

---
+++ assets/AppWrapper-4a262390.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-14e36fce.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-a2d1c570.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-02b0628e.js";const s=()=>t.jsx(r,{});export{s as default};
