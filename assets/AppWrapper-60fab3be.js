0f30b8265a2093e041afed2842bb81997f1e585e
shahsajib95 <sajib.shahariar97@gmail.com>
2026-01-23 21:03:52 +0600

    fix: reassign evaluator

--- assets/AppWrapper-60fab3be.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-5ccd9536.js";const s=()=>t.jsx(r,{});export{s as default};
---
+++ assets/AppWrapper-7af38ae2.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-6aca9c37.js";const s=()=>t.jsx(r,{});export{s as default};
