ef9860f4f45aae6b586df8997ab05cf81d860231
sp2703 <sudeepkalpas@gmail.com>
2026-02-26 12:33:26 +0530

    bugs fixed

---
+++ assets/AppWrapper-06795b0f.js
@@ -0,0 +1,1 @@
+import{j as t,O as r}from"./index-51d95d4d.js";const s=()=>t.jsx(r,{});export{s as default};
--- assets/AppWrapper-63b89cc2.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-cd658f1c.js";const s=()=>t.jsx(r,{});export{s as default};
